37f0c4073a8d0de13f6067d7665daa498dad14fc
Florian Lahitte <florianlahitte@MacBook-Air-de-Florian.local>
2016-09-16 17:11:32 +0200

    Add Avatar to User

---
+++ app/controllers/users_controllers.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.create( user_params )
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @User.update(params[:user].permit( users_params )
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+  private
+
+  # Use strong_parameters for attribute whitelisting
+  # Be sure to update your create() and update() controller methods.
+
+  def user_params
+    params.require(:user).permit(:avatar,:email)
+  end
+
+end
